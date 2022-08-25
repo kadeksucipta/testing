34d651a3a6a5f8ec69655e77ab3a8c83b573ff35
kadek12345 <kadek.sucipta005@gmail.com>
2022-08-25 12:19:40 +0800

    text update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Kadek Sucipta
+          <br />
+          MERN - Class
         </p>
         <a
           className="App-link"
